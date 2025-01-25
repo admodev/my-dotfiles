1761e45d5c507936dcc1ed53281c71335f4d538a
Adolfo Moyano <adolfohectormoyano@gmail.com>
2025-01-25 12:31:18 -0300

    Update configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -11,74 +11,79 @@
       <home-manager/nixos>
     ];
 
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
   };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "oslo"; # Define your hostname.                                  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "oslo"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
-                                                                                         # Set your time zone.
+
+  # Set your time zone.
   time.timeZone = "America/Argentina/Buenos_Aires";
-                                                                                         # Select internationalisation properties.
+
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_AR.UTF-8";                                                            LC_IDENTIFICATION = "es_AR.UTF-8";
+    LC_ADDRESS = "es_AR.UTF-8";
+    LC_IDENTIFICATION = "es_AR.UTF-8";
     LC_MEASUREMENT = "es_AR.UTF-8";
-    LC_MONETARY = "es_AR.UTF-8";                                                           LC_NAME = "es_AR.UTF-8";
-    LC_NUMERIC = "es_AR.UTF-8";
-    LC_PAPER = "es_AR.UTF-8";
-    LC_TELEPHONE = "es_AR.UTF-8";
-    LC_TIME = "es_AR.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.                       services.xserver.enable = true;
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";                                                                         variant = "";
-  };
-  # Enable CUPS to print documents.                                                      services.printing.enable = true;
+    LC_MONETARY = "es_AR.UTF-8";
+    LC_NAME = "es_AR.UTF-8";                                                               LC_NUMERIC = "es_AR.UTF-8";
+    LC_PAPER = "es_AR.UTF-8";                                                              LC_TELEPHONE = "es_AR.UTF-8";
+    LC_TIME = "es_AR.UTF-8";                                                             };
+                                                                                         # Enable the X11 windowing system.                                                     # You can disable this if you're only using the Wayland session.                       services.xserver.enable = true;
+                                                                                         # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;                                            services.desktopManager.plasma6.enable = true;
+                                                                                         # Configure keymap in X11
+  services.xserver.xkb = {                                                                 layout = "us";
+    variant = "";                                                                        };
+                                                                                         # Enable CUPS to print documents.
+  services.printing.enable = true;
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;                                                          services.pipewire = {
-    enable = true;                                                                         alsa.enable = true;
-    alsa.support32Bit = true;                                                              pulse.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
     # If you want to use JACK applications, uncomment this                                 #jack.enable = true;
-                                                                                           # use the example session manager (no others are packaged yet so this is enabled by default,                                                                                  # no need to redefine it in your config for now)
-    #media-session.enable = true;                                                        };                                                                                                                                                                            # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-  # Define a user account. Don't forget to set a password with ‘passwd’.                 users.users.admodevops = {
-    isNormalUser = true;                                                                   description = "Adolfo Moyano";                                                         extraGroups = [ "networkmanager" "wheel" "docker" "dialout" ];                         packages = with pkgs; [
-      kdePackages.kate                                                                     #  thunderbird
-    ];                                                                                     shell = pkgs.bash;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-  # Allow unfree packages                                                                nixpkgs.config.allowUnfree = true;
+  # Enable touchpad support (enabled default in most desktopManager).                    # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.admodevops = {                                                               isNormalUser = true;                                                                   description = "Adolfo Moyano";                                                         extraGroups = [ "networkmanager" "wheel" "docker" "dialout" ];
+    packages = with pkgs; [                                                                  kdePackages.kate
+    #  thunderbird                                                                         ];
+    shell = pkgs.bash;
+  };
+
+  # Install firefox.                                                                     programs.firefox.enable = true;
+                                                                                         # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # For packages with versions, like installing go 1.22 I personally use the explicit version on my user packages section, like this: go_1_22, so I can change versions seamlessly
-  home-manager.users.admodevops = { pkgs, ... }: {                                         home.packages = with pkgs; [
-        home-manager
-        ripgrep                                                                                fd
+  # $ nix search wget                                                                    # For packages with versions, like installing go 1.22 I personally use the explicit version on my user packages section, like this: go_1_22, so I can change versions seamlessly                                                                                     home-manager.users.admodevops = { pkgs, ... }: {
+    home.packages = with pkgs; [
+        home-manager                                                                           ripgrep
+        fd
         bat
         eza
         zig
@@ -88,13 +93,13 @@
         clojure
         leiningen
         julia
-        elixir
-        erlang                                                                                 nodejs_22
-        go_1_22                                                                            ];
-                                                                                           home.stateVersion = "25.05";
-  };
-  environment.systemPackages = with pkgs; [                                                bash
-    bash-completion                                                                        curl
+        elixir                                                                                 erlang
+        nodejs_22                                                                              go_1_22
+    ];
+    home.stateVersion = "25.05";                                                         };
+                                                                                         environment.systemPackages = with pkgs; [
+    bash                                                                                   bash-completion
+    curl
     wget
     gcc
     openjdk21
@@ -121,13 +126,13 @@
     # Add system-wide shell initialization here
   '';
   environment.interactiveShellInit = ''
-    if [ ! -d "$HOME/.oh-my-bash" ]; then
-      bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+    if [ ! -d "$HOME/.oh-my-bash" ]; then                                                    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
     fi
   '';
   environment.shellAliases = {
     ll = "ls -l";
     la = "ls -la";
+    edit-config = "sudo nvim /etc/nixos/configuration.nix";
     update-system = "sudo nixos-rebuild switch";
     update = "sudo nix-channel --update";
     delete-garbage = "sudo nix-collect-garbage -d";
